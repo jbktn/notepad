166b27503d0c7799818e2a844ce131d7b1fce27e
Jakub Pomorski <delveller1@gmail.com>
2023-10-07 15:15:18 +0200

    post-initial tweaks

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-
+  import login from './components/login.vue'
+  import register from './components/register.vue'
 </script>
 
 <template>
   <header>
-
+    <div class="headtext">Notepad</div>
   </header>
 
   <main>
@@ -13,5 +14,17 @@
 </template>
 
 <style scoped>
-
+  header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 10%;
+    background-color: rgb(33,51,33);
+    overflow: hidden;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 9999;
+    font-size: larger;
+  }
 </style>
